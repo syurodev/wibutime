fb905a767595679c661815ac3cde410505fdedcf
syurodev <syuro.dev@gmail.com>
2024-04-19 22:07:01 +0700

    feat: top views

--- components/page/Home/TopView/TopViews.tsx
+++ components/page/Home/TopView/TopViews.tsx
@@ -3,48 +3,82 @@
 import React, { useState } from "react";
 
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import ContentCard from "@/components/shared/Card/CardContent";
+import { AnimatePresence } from "framer-motion";
+import DivSlide from "@/components/shared/Animation/DivSlide";
 
-const TopViews = () => {
+type IProps = {
+  data: {
+    animes: ContentCardType[];
+    mangas: ContentCardType[];
+    lightnovels: ContentCardType[];
+  };
+};
+
+const TopViews: React.FC<IProps> = ({ data }) => {
   const [contentType, setContentType] = useState<ContentType>("anime");
 
   return (
-    <div>
-      <div>
-        <h2>Top Views</h2>
-        <div>
-          <ToggleGroup
-            type="single"
-            // defaultValue={contentType}
-            value={contentType}
-            onValueChange={(e) =>
-              setContentType(!e ? "anime" : (e as ContentType))
-            }
+    <Card>
+      <CardHeader className="flex items-center justify-between flex-row">
+        <CardTitle>Top Views</CardTitle>
+        <ToggleGroup
+          type="single"
+          // defaultValue={contentType}
+          value={contentType}
+          onValueChange={(e) =>
+            setContentType(!e ? "anime" : (e as ContentType))
+          }
+          className="!mt-0"
+        >
+          <ToggleGroupItem
+            value="anime"
+            aria-label="Toggle anime"
+            className="rounded-full !mt-0"
           >
-            <ToggleGroupItem
-              value="anime"
-              aria-label="Toggle anime"
-              className="rounded-full"
-            >
-              Anime
-            </ToggleGroupItem>
-            <ToggleGroupItem
-              value="manga"
-              aria-label="Toggle manga"
-              className="rounded-full"
-            >
-              Manga
-            </ToggleGroupItem>
-            <ToggleGroupItem
-              value="lightnovel"
-              aria-label="Toggle lightnovel"
-              className="rounded-full"
-            >
-              Lightnovel
-            </ToggleGroupItem>
-          </ToggleGroup>
-        </div>
-      </div>
-    </div>
+            Anime
+          </ToggleGroupItem>
+          <ToggleGroupItem
+            value="manga"
+            aria-label="Toggle manga"
+            className="rounded-full !mt-0"
+          >
+            Manga
+          </ToggleGroupItem>
+          <ToggleGroupItem
+            value="lightnovel"
+            aria-label="Toggle lightnovel"
+            className="rounded-full !mt-0"
+          >
+            Lightnovel
+          </ToggleGroupItem>
+        </ToggleGroup>
+      </CardHeader>
+      <CardContent className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 items-center">
+        <AnimatePresence mode="wait">
+          {contentType === "anime"
+            ? data.animes.map((item, index) => (
+                <DivSlide key={item.url_id} delay={0.05 + index / 5}>
+                  <ContentCard data={item} />
+                </DivSlide>
+              ))
+            : contentType === "manga"
+            ? data.mangas.map((item, index) => (
+                <DivSlide key={item.url_id} delay={0.05 + index / 5}>
+                  <ContentCard data={item} />
+                </DivSlide>
+              ))
+            : contentType === "lightnovel"
+            ? data.lightnovels.map((item, index) => (
+                <DivSlide key={item.url_id} delay={0.05 + index / 5}>
+                  <ContentCard data={item} />
+                </DivSlide>
+              ))
+            : null}
+        </AnimatePresence>
+      </CardContent>
+    </Card>
   );
 };
 
