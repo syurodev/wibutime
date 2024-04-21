429caa8ad1c5afb6ae3c63fccbf6ff6e3345c3dc
syurodev <syuro.dev@gmail.com>
2024-04-21 15:17:24 +0700

    feat: theme system

--- components/shared/Button/ThemeToggle.tsx
+++ components/shared/Button/ThemeToggle.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { useTheme } from "next-themes";
-import { LuMoon, LuSun } from "react-icons/lu";
+import { LuMonitor, LuMoon, LuSun } from "react-icons/lu";
 
 import { Button } from "@/components/ui/button";
 
@@ -19,20 +19,32 @@ const ThemeToggle: React.FC<IProps> = ({ size = "default", blur = false }) => {
       variant={"outline"}
       rounded={"full"}
       size={size}
-      onClick={() => (theme === "dark" ? setTheme("light") : setTheme("dark"))}
+      onClick={() =>
+        theme === "dark"
+          ? setTheme("system")
+          : theme === "system"
+          ? setTheme("light")
+          : setTheme("dark")
+      }
       className={blur ? "bg-background/30" : "bg-background"}
     >
       {theme === "dark" ? (
         <LuMoon
-          className={`text-base rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 right-0 ${
+          className={`text-base rotate-90 scale-0 transition-all dark:rotate-0 transition-none duration-150 dark:scale-100 right-0 ${
+            size !== "icon" ? "mr-2" : ""
+          }`}
+        />
+      ) : theme === "light" ? (
+        <LuSun
+          className={`text-base rotate-0 scale-100 transition-all dark:-rotate-90 transition-none duration-150 dark:scale-0 ${
             size !== "icon" ? "mr-2" : ""
           }`}
         />
       ) : (
-        <LuSun
-          className={`text-base rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 ${
+        <LuMonitor
+          className={`text-base rotate-0 transition-all ${
             size !== "icon" ? "mr-2" : ""
-          }`}
+          } ${theme === "system" ? "scale-100" : "scale-0"}`}
         />
       )}
 
