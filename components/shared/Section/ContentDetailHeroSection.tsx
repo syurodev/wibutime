fb92a670518729ad4bf3e0a368c103f60a3679fb
syurodev <syuro.dev@gmail.com>
2024-04-24 22:15:33 +0700

    style: remove detail contenr

--- components/shared/Section/ContentDetailHeroSection.tsx
+++ components/shared/Section/ContentDetailHeroSection.tsx
@@ -29,24 +29,7 @@ type IProps = {
     score: number;
     favorites: number;
     views: number;
-    order_name: string[];
-    summary: string;
     content_type: ContentType;
-    note?: string;
-
-    //user
-    user: {
-      user_id: number;
-      name: string;
-      image?: string;
-    };
-
-    //translate group
-    translate_group: {
-      translate_group_id: number;
-      translate_group_name: string;
-      image?: string;
-    };
 
     //lightnovel
     author?: string;
@@ -54,13 +37,6 @@ type IProps = {
     words?: number;
   };
 };
-
-const order_name = [
-  "高校時代に傲慢だった女王様との同棲生活は意外と居心地が悪くない",
-  "Koukou Jidai ni Gouman datta Joou-sama to no Dousei Seikatsu wa Igaito Igokochi ga Warukunai",
-  "Living with the Arrogant Queen from High School is Surprisingly Not Uncomfortable",
-  "Sống chung với Nữ hoàng kiêu ngạo",
-];
 
 const categories = [
   {
@@ -77,233 +53,141 @@ const content_type = "lightnovel";
 
 const ContentDetailHeroSection = () => {
   return (
-    <div className="flex flex-col gap-4 h-dvh overflow-y-auto px-4 pt-[82px] w-full max-w-[1300px] mx-auto pb-4">
-      <div className="flex flex-col gap-4 md:flex-row">
-        <div className="relative w-[300px] h-[450px] mx-auto rounded-lg overflow-hidden shadow flex-none">
-          <Image
-            src={
-              "https://images.unsplash.com/photo-1713109510454-90f68a4269f1?q=80&w=1921&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            }
-            alt=""
-            fill
-            priority
-            sizes="full"
-            className="object-cover"
-          />
+    <div className="flex flex-col md:flex-row gap-4 flex-none relative">
+      <DivSlide className="relative w-[300px] h-[450px] mx-auto rounded-lg overflow-hidden shadow flex-none">
+        <Image
+          src={
+            "https://images.unsplash.com/photo-1713109510454-90f68a4269f1?q=80&w=1921&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          }
+          alt=""
+          fill
+          priority
+          sizes="full"
+          className="object-cover"
+        />
+      </DivSlide>
+
+      <div className="flex flex-col gap-4">
+        {/* Tiêu đề */}
+        <H2Slide
+          delay={0.15}
+          className="font-semibold text-pretty text-2xl lg:text-3xl font-serif text-center md:text-left"
+        >
+          Sống chung với Nữ hoàng kiêu ngạo thời cao trung hoá ra lại không hề
+          khó chịu một cách đáng ngạc nhiên
+        </H2Slide>
+
+        {/* Thể loại */}
+        <div className="flex flex-row gap-2 items-start flex-wrap justify-center md:justify-start">
+          {categories.map((category, index) => {
+            return (
+              <BadgeLink
+                key={`category-${category.id}`}
+                url={`/search?type=${content_type}&categories=${encodeURIComponent(
+                  category.name
+                )}`}
+                variant={"outline"}
+                delay={0.2 + index * 0.05}
+              >
+                {category.name}
+              </BadgeLink>
+            );
+          })}
         </div>
 
-        <div className="flex flex-col gap-4">
-          {/* Tiêu đề */}
-          <H2Slide
-            delay={0.15}
-            className="font-semibold text-pretty text-2xl lg:text-3xl font-serif text-center md:text-left"
-          >
-            Sống chung với Nữ hoàng kiêu ngạo thời cao trung hoá ra lại không hề
-            khó chịu một cách đáng ngạc nhiên
-          </H2Slide>
-
-          {/* Thể loại */}
-          <div className="flex flex-row gap-2 items-start flex-wrap justify-center md:justify-start">
-            {categories.map((category, index) => {
-              return (
-                <BadgeLink
-                  key={`category-${category.id}`}
-                  url={`/search?type=${content_type}&categories=${encodeURIComponent(
-                    category.name
-                  )}`}
-                  variant={"outline"}
-                  delay={0.2 + index * 0.05}
-                >
-                  {category.name}
-                </BadgeLink>
-              );
-            })}
-          </div>
-
-          <DivSlide delay={0.35} className="h-fit">
-            <Card className="flex flex-col gap-3 p-4">
-              {/* Tác giả */}
-              <h3 className="text-sm">
-                <span className="font-semibold">Tác giả: </span>{" "}
-                <LinkButton
-                  url={`/search?type=lightnovel&author=${encodeURIComponent(
-                    "Misoneta Dozaemon (ミソネタ・ドざえもん)"
-                  )}`}
-                >
-                  Misoneta Dozaemon (ミソネタ・ドざえもん)
-                </LinkButton>
-              </h3>
-
-              {/* Hoạ sĩ */}
-              <h3 className="text-sm">
-                <span className="font-semibold">Hoạ sĩ: </span>
-                <LinkButton
-                  url={`/search?type=lightnovel&illustrator=${encodeURIComponent(
-                    "Yuga (ゆが一)"
-                  )}`}
-                >
-                  Yuga (ゆが一)
-                </LinkButton>
-              </h3>
-
-              {/* Trạng thái */}
-              <h3 className="text-sm">
-                <span className="font-semibold">Trạng thái: </span>
-                <LinkButton url={`/search?type=lightnovel&status=inprocess`}>
-                  Đang tiến hành
-                </LinkButton>
-              </h3>
-
-              {/* Ngày tạo */}
-              <h3 className="text-sm">
-                <span className="font-semibold">Ngày tạo: </span>
-                {new Date(1711099616608).toDateString()}
-              </h3>
-
-              {/* Cập nhật lần cuối */}
-              <h3 className="text-sm">
-                <span className="font-semibold">Cập nhật lần cuối: </span>
-                {new Date(1713099616608).toDateString()}
-              </h3>
-            </Card>
-          </DivSlide>
-
-          <DivSlide delay={0.4} className="h-fit">
-            <Card className="flex items-center justify-between w-full p-4">
-              {/* Số từ */}
-              <div className="flex flex-col items-center justify-center w-full">
-                <LuCaseSensitive />
-                <p className="font-medium text-xs">{numberFormat(32131421)}</p>
-              </div>
-
-              {/* Số lượt xem */}
-              <div className="flex flex-col items-center justify-center w-full">
-                <LuEye />
-                <p className="font-medium text-xs">{numberFormat(32131421)}</p>
-              </div>
-
-              {/* Số bình luận */}
-              <Button
-                className="flex flex-col items-center justify-center w-full p-0"
-                variant={"ghost"}
-                rounded={"lg"}
+        <DivSlide delay={0.35} className="h-fit">
+          <Card className="flex flex-col gap-3 p-4">
+            {/* Tác giả */}
+            <h3 className="text-sm">
+              <span className="font-semibold">Tác giả: </span>{" "}
+              <LinkButton
+                url={`/search?type=lightnovel&author=${encodeURIComponent(
+                  "Misoneta Dozaemon (ミソネタ・ドざえもん)"
+                )}`}
               >
-                <LuMessageCircle />
-                <p className="font-medium text-xs">{numberFormat(120)}</p>
-              </Button>
+                Misoneta Dozaemon (ミソネタ・ドざえもん)
+              </LinkButton>
+            </h3>
 
-              {/* Số yêu thích */}
-              <Button
-                className="flex flex-col items-center justify-center w-full p-0"
-                variant={"ghost"}
-                rounded={"lg"}
+            {/* Hoạ sĩ */}
+            <h3 className="text-sm">
+              <span className="font-semibold">Hoạ sĩ: </span>
+              <LinkButton
+                url={`/search?type=lightnovel&illustrator=${encodeURIComponent(
+                  "Yuga (ゆが一)"
+                )}`}
               >
-                <LuHeart />
-                <p className="font-medium text-xs">{numberFormat(31421)}</p>
-              </Button>
+                Yuga (ゆが一)
+              </LinkButton>
+            </h3>
 
-              {/* Số điểm đánh giá */}
-              <Button
-                className="flex flex-col items-center justify-center w-full p-0"
-                variant={"ghost"}
-                rounded={"lg"}
-              >
-                <LuStar />
-                <p className="font-medium text-xs">4.5</p>
-              </Button>
-            </Card>
-          </DivSlide>
-        </div>
-      </div>
+            {/* Trạng thái */}
+            <h3 className="text-sm">
+              <span className="font-semibold">Trạng thái: </span>
+              <LinkButton url={`/search?type=lightnovel&status=inprocess`}>
+                Đang tiến hành
+              </LinkButton>
+            </h3>
 
-      <div className="flex flex-col gap-4 md:flex-row">
-        <div className="space-y-4">
-          {/* Tên khác */}
-          <DivSlide delay={0.45} className="h-fit">
-            <Card>
-              <CardHeader>
-                <CardTitle className="text-sm">Tên khác</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <ul className="list-disc">
-                  {order_name.map((item) => {
-                    return (
-                      <li key={item} className="text-sm ml-4 mb-2 last:mb-0">
-                        {item}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </CardContent>
-            </Card>
-          </DivSlide>
+            {/* Ngày tạo */}
+            <h3 className="text-sm">
+              <span className="font-semibold">Ngày tạo: </span>
+              {new Date(1711099616608).toDateString()}
+            </h3>
 
-          {/* Tóm tắt */}
-          <DivSlide delay={0.5} className="h-fit">
-            <Card>
-              <CardHeader>
-                <CardTitle className="text-sm">Tóm tắt</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p className="text-sm">
-                  {`Vào đêm khuya của một ngày nọ, Yamamoto - một sinh viên đại học đang làm việc bán thời gian tại một cửa hàng tiện lợi bắt gặp một khách hàng nữ mang trong mình bộ đồ nỉ tại quầy thu ngân.
+            {/* Cập nhật lần cuối */}
+            <h3 className="text-sm">
+              <span className="font-semibold">Cập nhật lần cuối: </span>
+              {new Date(1713099616608).toDateString()}
+            </h3>
+          </Card>
+        </DivSlide>
 
-              Đó là Megumi Hayashi, là bạn cũng lớp với Yamamoto thời cao trung và cũng là cô gái xinh đẹp nhất lớp. Với tính cách hiếu thắng và cao ngạo của mình , cô nàng được mọi người gọi với biệt danh "Nữ hoàng".
-
-              Trong khi trò chuyện tầm phào về công việc cùng Hayashi-san, người mà cậu không thực sự quá thân thiết ở trường, Yamamoto nhận thấy một vết bầm tím đau đớn trên tay cô.
-
-              Biết được nguồn cơn của vết thương này là từ bạn trai của cô nàng, cậu quyết định đưa cô về nhà một khoảng thời gian và che giấu cô ấy.
-
-              Bỗng nhiên, vào khoảnh khắc Yamamoto chuẩn bị đi ngủ, cậu bất ngờ bị Hayashi-san ôm từ phía sau...!?
-
-              Điều gì sẽ xảy ra đối với cuộc sống chung của cậu và "Nữ hoàng" bị tổn thương, được bắt đầu từ mối quan hệ tồi tệ nhất...!?`}
-                </p>
-              </CardContent>
-            </Card>
-          </DivSlide>
-        </div>
-
-        <Card className="min-w-[300px] h-fit">
-          <CardHeader className="flex flex-row gap-2 items-center">
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-
-            <div className="!mt-0">
-              <p className="text-sm font-semibold">user name</p>
-              <p className="text-xs font-medium">translate group</p>
+        <DivSlide delay={0.4} className="h-fit">
+          <Card className="flex items-center justify-between w-full p-4">
+            {/* Số từ */}
+            <div className="flex flex-col items-center justify-center w-full">
+              <LuCaseSensitive />
+              <p className="font-medium text-xs">{numberFormat(32131421)}</p>
             </div>
-          </CardHeader>
-          <CardContent>
-            <p className="text-sm font-semibold">Ghi chú:</p>
 
-            <div className="text-sm">
-              <p>Tiến độ: không xác định</p>
-              <p>
-                Đây là project đầu tay của mình. Hy vọng các bạn sẽ có thể đưa
-                ra lời đánh giá cả tích cực lẫn tiêu cực để mình có thể hoàn
-                thiện bộ truyện này nhé.
-              </p>
-              <p>
-                Vì đây là dịch trực tiếp nên có thể văn phong sẽ hơi lủng củng.
-              </p>
-              <p>
-                <i>Mọi sự ủng hộ sẽ góp phần cổ vũ mình dịch nhanh hơn:</i>
-              </p>
-              <b>1. STK: 102876833996 VIETIN - PHUNG TUAN DAT </b>
-              <b>2. Các bạn có thể ủng hộ mình 1 follow tại:</b>
-              <p>-----------------------</p>
-              <p>Theo dõi trang cá nhân của tác giả và họa sĩ tại:</p>
-              <b>
-                Tác giả Misoneta Dozaemon:{" "}
-                <a href="https://x.com/jAHFrtti5CzkfTG">
-                  https://x.com/jAHFrtti5CzkfTG
-                </a>
-              </b>
+            {/* Số lượt xem */}
+            <div className="flex flex-col items-center justify-center w-full">
+              <LuEye />
+              <p className="font-medium text-xs">{numberFormat(32131421)}</p>
             </div>
-          </CardContent>
-        </Card>
+
+            {/* Số bình luận */}
+            <Button
+              className="flex flex-col items-center justify-center w-full p-0"
+              variant={"ghost"}
+              rounded={"lg"}
+            >
+              <LuMessageCircle />
+              <p className="font-medium text-xs">{numberFormat(120)}</p>
+            </Button>
+
+            {/* Số yêu thích */}
+            <Button
+              className="flex flex-col items-center justify-center w-full p-0"
+              variant={"ghost"}
+              rounded={"lg"}
+            >
+              <LuHeart />
+              <p className="font-medium text-xs">{numberFormat(31421)}</p>
+            </Button>
+
+            {/* Số điểm đánh giá */}
+            <Button
+              className="flex flex-col items-center justify-center w-full p-0"
+              variant={"ghost"}
+              rounded={"lg"}
+            >
+              <LuStar />
+              <p className="font-medium text-xs">4.5</p>
+            </Button>
+          </Card>
+        </DivSlide>
       </div>
     </div>
   );
