60661e37206890d21c3880d237dee545756434c8
syurodev <syuro.dev@gmail.com>
2025-04-03 11:30:59 +0700

    Cập nhật tệp chapter.entity.ts để thêm thuộc tính lang và điều chỉnh định dạng mã. Thêm xuất lang.enum vào tệp index.ts trong thư viện types. Đã cải thiện cấu trúc mã nguồn và loại bỏ mã thừa. Chưa hoàn thành việc kiểm tra và tối ưu hóa một số thành phần trong ứng dụng.

---
+++ packages/types/src/defaults/lang.enum.ts
@@ -0,0 +1,11 @@
+export enum LANG {
+  VI = 1,
+  EN = 2,
+  JA = 3,
+}
+
+export const LANG_LABEL = {
+  [LANG.VI]: 'Vietnamese',
+  [LANG.EN]: 'English',
+  [LANG.JA]: 'Japanese',
+};
--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -4,5 +4,6 @@ export * from './auth/session';
 
 export * from './defaults/content-type.enum';
 export * from './defaults/editor-content';
+export * from './defaults/lang.enum';
 export * from './defaults/top-type.enum';
 export * from './defaults/version-controller.enum';
